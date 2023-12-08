689dee128330f0005f90958c5d11ec5a6c39e412
Ulrich Kramer <u.kramer@sap.com>
2023-12-08 14:35:46 +0100

    Change build.sh to new version

--- build.sh
+++ build.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 set -e
+VERSION=1.36.0
 
 ./gradlew clean publishToMavenLocal
 mvn install:install-file \
-   -Dfile=core/build/libs/calcite-core-1.35.0-SNAPSHOT.jar \
+   -Dfile=core/build/libs/calcite-core-$VERSION-SNAPSHOT.jar \
    -DgroupId=org.apache.calcite \
    -DartifactId=calcite-core-sap \
-   -Dversion=1.35.0 \
+   -Dversion=$VERSION \
    -Dpackaging=jar \
    -DlocalRepositoryPath=../nucleus-resources-app/patches/
 mvn install:install-file \
-   -Dfile=core/build/libs/calcite-core-1.35.0-SNAPSHOT.jar \
+   -Dfile=core/build/libs/calcite-core-$VERSION-SNAPSHOT.jar \
    -DgroupId=org.apache.calcite \
    -DartifactId=calcite-core-sap \
-   -Dversion=1.35.0 \
+   -Dversion=$VERSION \
    -Dpackaging=jar \
    -DlocalRepositoryPath=$HOME/.m2/repository
+cat core/build/publications/core/pom-default.xml | \
+  sed -e 's/calcite-core/calcite-core-sap/g' -e "s/${VERSION}-SNAPSHOT/${VERSION}/g" > \
+  ../nucleus-resources-app/patches/org/apache/calcite/calcite-core-sap/$VERSION/calcite-core-sap-$VERSION.pom
