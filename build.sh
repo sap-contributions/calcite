6120f311cbb610c62a0b88c18b111e401d6b9e95
Ulrich Kramer <u.kramer@sap.com>
2024-02-07 14:58:16 +0100

    Add linq4j to build.sh

--- build.sh
+++ build.sh
@@ -4,20 +4,24 @@ set -e
 VERSION=1.36.0
 
 ./gradlew clean publishToMavenLocal
-mvn install:install-file \
-   -Dfile=core/build/libs/calcite-core-$VERSION-SNAPSHOT.jar \
-   -DgroupId=org.apache.calcite \
-   -DartifactId=calcite-core-sap \
-   -Dversion=$VERSION \
-   -Dpackaging=jar \
-   -DlocalRepositoryPath=../nucleus-resources-app/patches/
-mvn install:install-file \
-   -Dfile=core/build/libs/calcite-core-$VERSION-SNAPSHOT.jar \
-   -DgroupId=org.apache.calcite \
-   -DartifactId=calcite-core-sap \
-   -Dversion=$VERSION \
-   -Dpackaging=jar \
-   -DlocalRepositoryPath=$HOME/.m2/repository
-cat core/build/publications/core/pom-default.xml | \
-  sed -e 's/calcite-core/calcite-core-sap/g' -e "s/${VERSION}-SNAPSHOT/${VERSION}/g" > \
-  ../nucleus-resources-app/patches/org/apache/calcite/calcite-core-sap/$VERSION/calcite-core-sap-$VERSION.pom
+
+for module in core linq4j;
+do
+   mvn install:install-file \
+      -Dfile=${module}/build/libs/calcite-${module}-$VERSION-SNAPSHOT.jar \
+      -DgroupId=org.apache.calcite \
+      -DartifactId=calcite-${module}-sap \
+      -Dversion=$VERSION \
+      -Dpackaging=jar \
+      -DlocalRepositoryPath=../nucleus-resources-app/patches/
+   mvn install:install-file \
+      -Dfile=${module}/build/libs/calcite-${module}-$VERSION-SNAPSHOT.jar \
+      -DgroupId=org.apache.calcite \
+      -DartifactId=calcite-${module}-sap \
+      -Dversion=$VERSION \
+      -Dpackaging=jar \
+      -DlocalRepositoryPath=$HOME/.m2/repository
+   cat ${module}/build/publications/${module}/pom-default.xml | \
+   sed -e 's/calcite-${module}/calcite-${module}-sap/g' -e "s/${VERSION}-SNAPSHOT/${VERSION}/g" > \
+   ../nucleus-resources-app/patches/org/apache/calcite/calcite-${module}-sap/$VERSION/calcite-${module}-sap-$VERSION.pom
+done
