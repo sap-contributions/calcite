7ad1b1993f30eff6794ccdf990dfc96fb711fdf2
Ulrich Kramer <u.kramer@sap.com>
2025-09-18 14:04:11 +0200

    fix: Fix Return code of LISTAGG

--- core/src/main/java/org/apache/calcite/sql/fun/SqlStdOperatorTable.java
+++ core/src/main/java/org/apache/calcite/sql/fun/SqlStdOperatorTable.java
@@ -2410,7 +2410,7 @@ public class SqlStdOperatorTable extends ReflectiveSqlOperatorTable {
    * The LISTAGG operator. String aggregator function.
    */
   public static final SqlAggFunction LISTAGG =
-      new SqlListaggAggFunction(SqlKind.LISTAGG, ReturnTypes.ARG0_NULLABLE);
+      new SqlListaggAggFunction(SqlKind.LISTAGG, ReturnTypes.VARCHAR);
 
   /**
    * The FUSION operator. Multiset aggregator function.
